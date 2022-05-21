3d1908028164ab1f8b2ca619a82cfb555a0a7fa6
ZAID-CAIDI <zaidcaidi14casa@gmail.com>
2022-05-21 21:05:35 +0100

    edit web

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,10 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to atelier Cloud Computing <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <h1>Bonjour : zaidcaidi@gmail.com</h1>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
